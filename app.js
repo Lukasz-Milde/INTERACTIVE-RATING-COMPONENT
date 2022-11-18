2ec5b782fef07265c774fc637e38f4606a953974
Lukasz-Milde <lukasz@milde.live>
2022-11-18 15:35:55 +0100

    initial commit with js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const name = "dupa";
+const submitBtn = document.querySelector(".btn-submit");
+const stateOne = document.querySelector(".card-state-1");
+const stateTwo = document.querySelector("card-state-2");
+const ratingBtn = document.querySelectorAll("btn-rating");
+const score = document.querySelector("score");
+
+submitBtn.addEventListener("click", changeState);
+
+function changeState() {
+	stateTwo.classList.remove(".hide");
+	stateOne.classList.add(".hide");
+}
