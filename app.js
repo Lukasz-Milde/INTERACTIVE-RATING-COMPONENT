cd1e22abec50007fec9527240fe75c7297ff16b6
Lukasz-Milde <lukasz@milde.live>
2022-11-18 19:31:54 +0100

    cont with JS

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const submitBtn = document.querySelector(".btn-submit");
 const stateOne = document.querySelector(".card-state-1");
-const stateTwo = document.querySelector("card-state-2");
-const ratingBtn = document.querySelectorAll("btn-rating");
-const score = document.querySelector("score");
+const stateTwo = document.querySelector(".card-state-2");
+const ratingBtn = document.querySelectorAll(".btn-rating");
+let score = document.querySelector(".score");
 
 submitBtn.addEventListener("click", changeState);
 
 function changeState() {
-	stateTwo.classList.remove(".hide");
-	stateOne.classList.add(".hide");
+	stateOne.classList.add("hide");
+	stateTwo.classList.remove("hide");
+}
+
+ratingBtn.forEach((rate) => {
+	rate.addEventListener("click", handleRateClick);
+});
+
+function handleRateClick() {
+	ratingBtn.forEach((rate) => rate.classList.remove("active"));
 }
