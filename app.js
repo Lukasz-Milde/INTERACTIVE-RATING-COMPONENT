5849488173a04c0e9cf7ba5a491846f119a92168
Lukasz-Milde <lukasz@milde.live>
2022-11-19 12:09:46 +0100

    done

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const submitBtn = document.querySelector(".btn-submit");
 const stateOne = document.querySelector(".card-state-1");
 const stateTwo = document.querySelector(".card-state-2");
 const ratingBtn = document.querySelectorAll(".btn-rating");
-let score = document.querySelector(".score");
+const score = document.querySelector(".score");
+let finalScore = 4;
 
 submitBtn.addEventListener("click", changeState);
 
 function changeState() {
 	stateOne.classList.add("hide");
 	stateTwo.classList.remove("hide");
+	score.textContent = finalScore;
 }
 
 ratingBtn.forEach((rate) => {
 	rate.addEventListener("click", handleRateClick);
 });
 
-function handleRateClick() {
+function handleRateClick(event) {
 	ratingBtn.forEach((rate) => rate.classList.remove("active"));
+	event.target.classList.add("active");
+	finalScore = event.target.textContent;
 }
